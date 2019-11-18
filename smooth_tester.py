10cc11a6a6c25bf10d2bf7d4dceae530b6fa0058
ems236 <ems236@case.edu>
2019-11-17 19:43:03 -0500

    functioning is smooth

--- smooth_tester.py
+++ smooth_tester.py
@@ -3,26 +3,30 @@ class SmoothResidueSieve:
         self.data = [(num**2 - n) for num in range(0, n + 1)]
         self.max_value = n
 
-
         for prime in primes:
             # solve x^2 - n == 0 mod p
             quadratic_zeros = quadratic_zeros_mod_p(n, prime)
-            if len(quadratic_zeros) == 2:
+            if quadratic_zeros:
                 for quadratic_zero in quadratic_zeros:
-                    current_index = quadratic_zero
-                    while current_index >= self.max_value:
-                        self.decrement_value(current_index, prime)
-                        current_index = current_index + prime
+                    self.sieve_for_zero(quadratic_zero, prime)
+
+    def sieve_for_zero(self, quadratic_zero, prime):
+        current_index = quadratic_zero
+        while current_index <= self.max_value:
+            self.decrement_value(current_index, prime)
+            current_index = current_index + prime
 
     def decrement_value(self, index, prime):
-        if self.data[index] % prime == 0:
+        print(f"decrementing {index} for prime {prime}, value is {self.data[index]}")
+        while self.data[index] > 0 and self.data[index] % prime == 0 :
             self.data[index] = self.data[index] // prime
+        print(f"value is now {self.data[index]} \n")
 
     def is_qr_smooth(self, a):
         return self.data[a] == 1
 
-    @staticmethod
-    def quadratic_zeros_mod_p(n, p):
+    
+def quadratic_zeros_mod_p(n, p):
         #brute force because you assume p is small
         solutions = []
         for num in range(0, p):
