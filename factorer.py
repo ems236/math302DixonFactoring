ac9cb6901bd1f0fcd27ba2e2b315b705f49182bb
ems236 <ems236@case.edu>
2019-11-17 22:32:32 -0500

    cleaned up root calculation

--- factorer.py
+++ factorer.py
@@ -16,17 +16,19 @@ def factor(number):
     if has_factor:
         return a, b
     
-    #generate pi(B) random smooth residues numbers in the correct range
-    smooth_residue_list, source_list = smooth_residues(number, selection_bound, primes)
-    #do guassian elimination on the factor exponent vectors to get another square
-    residue_vectors = [smooth_number_binary_prime_exponents(residue, primes) for residue in smooth_residue_list]
-    binary_residue_vectors = [exponent % 2 for exponent in residue_vectors]
-    perfect_square_indeces = 
+    #find a case of x^2 == y^2 mod n
+    attempts = 0
+    found = False
 
-    #solve x^2 == y^2 mod n
-    #find gcd(x-y, n)
-    #try again if it makes trivial factors
-    pass
+    while attempts < 50 and not found: 
+        first_root, second_root = roots_of_congruent_squares(number, selection_bound, primes)
+        if first_root % number not in [second_root % number, (-1 * second_root) % number]:
+            found = True
+            #find gcd(x-y, n)
+            gcd = math.gcd()
+            #try again if it makes trivial factors
+        attempts = attempts + 1
+    return 1, number
 
 def smoothness_parameters(number):
     logn = math.log(number)
@@ -44,6 +46,17 @@ def any_divide(number, primes):
         if number % prime == 0:
             return True, prime, number // prime
     return False, 0, 0
+
+def roots_of_congruent_squares(number, selection_bound, primes):
+    #generate pi(B) random smooth residues numbers in the correct range
+    smooth_residue_list, source_list = smooth_residues(number, selection_bound, primes)
+    #do guassian elimination on the factor exponent vectors to get another square
+    residue_vectors = [smooth_number_prime_exponents(residue, primes) for residue in smooth_residue_list]
+    binary_residue_vectors = [to_binary_exponent_vector(vector) for vector in residue_vectors]
+    perfect_square_indeces = subset_product_square_indeces(binary_residue_vectors)
+
+    #solve x^2 == y^2 mod n
+    return roots_for(source_list, residue_vectors, perfect_square_indeces)
 
 def smooth_residues(number, selection_bound, primes):
     smooth_sieve = SmoothResidueSieve(number, selection_bound, primes)
@@ -76,8 +89,30 @@ def exponent_divide(number, divisor):
     return count
 
 
-def smooth_number_binary_prime_exponents(number, primes):
+def smooth_number_prime_exponents(number, primes):
     return [exponent_divide(number, prime) for prime in primes]
+
+def to_binary_exponent_vector(vector):
+    return [exponent % 2 for exponent in vector]
+
+def subset_product_square_indeces(binary_residues):
+    pass
+
+def roots_for(source_list, residue_vectors, selected_indeces, primes):
+    first_root = 1
+    second_root = 1
+    second_primes_exponents = [0 for _ in primes]
+
+    for index in selected_indeces:
+        first_root = first_root * source_list[index]
+        for prime_index in range(0, len(primes)):
+            second_primes_exponents[prime_index] = second_primes_exponents[prime_index] + residue_vectors[index][prime_index]
+
+    for prime_index in range(0, len(primes)):
+        second_root = second_root * (primes[prime_index] ** (second_primes_exponents[prime_index] // 2))
+    
+    return first_root, second_root
+
 
 if __name__ == "__main__":
     if sys.argv > 1:
